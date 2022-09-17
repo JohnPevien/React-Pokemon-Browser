e2c98d11fdee2e23a4705c5f5947425aff39e4a6
John Pevien Laguindam <johnpevienwork@gmail.com>
2022-09-18 03:42:25 +0800

    Update Navigation component

--- components/Navigation.tsx
+++ components/Navigation.tsx
@@ -2,11 +2,19 @@ import Link from "next/link";
 type Props = {};
 export default function Navigation({}: Props) {
   return (
-    <header className="flex h-20 bg-indigo-900">
-      <div className="flex items-center justify-center flex-1 text-white">
-        <Link href="/">
-          <a className="text-4xl font-medium text-white">Pokemon Browser</a>
-        </Link>
+    <header className="flex  h-20 bg-indigo-900  text-white">
+      <div className="container flex items-center">
+        <div className="flex">
+          <Link href="/">
+            <a className="text-2xl font-medium text-white">Pokemon Browser</a>
+          </Link>
+        </div>
+        <div className="flex items-center justify-center flex-1 text-white"></div>
+        <div className=" flex items-center justify-end flex-1 text-white">
+          <Link href="/about">
+            <a className="text-base font-medium text-white">About</a>
+          </Link>
+        </div>
       </div>
     </header>
   );
