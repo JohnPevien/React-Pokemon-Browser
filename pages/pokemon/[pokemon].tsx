690f3502244ce8b035bbc7a3e2dc5cada048a18c
John Pevien Laguindam <johnpevien@JPL.localdomain>
2022-09-22 18:01:31 +0800

    Add display for stats and abilities

--- pages/pokemon/[pokemon].tsx
+++ pages/pokemon/[pokemon].tsx
@@ -1,3 +1,4 @@
+import { useEffect } from "react";
 import { GetStaticPaths, GetStaticProps } from "next";
 import Image from "next/image";
 type Props = {
@@ -18,6 +19,12 @@ export const getStaticPaths: GetStaticPaths = async () => {
   }));
 
   return { paths, fallback: false };
+};
+
+const getAbilityDetails = async (apiURL: string) => {
+  const res = await fetch(apiURL);
+  const ability = await res.json();
+  return ability.effect_entries[0].effect;
 };
 
 export const getStaticProps: GetStaticProps = async (context) => {
@@ -41,6 +48,62 @@ export const getStaticProps: GetStaticProps = async (context) => {
 };
 
 function Pokemon({ pokemon }: Props) {
-  return <div>{pokemon.name}</div>;
+  useEffect(() => {});
+  return (
+    <section>
+      <div className="flex justify-center">
+        <header className="flex w-full bg-indigo-500 p-5 rounded-xl text-white items-center justify-center drop-shadow-xl">
+          <div className="flex flex-row w-fit gap-5 items-center">
+            <Image
+              src={pokemon.sprites.back_default}
+              width={96}
+              height={100}
+              alt={pokemon.name}
+            />
+            <div className="flex flex-row items-center">
+              <h1 className="capitalize ">{pokemon.name}</h1>
+              <p className="ml-2"># {pokemon.order}</p>
+            </div>
+            <Image
+              src={pokemon.sprites.front_default}
+              width={96}
+              height={100}
+              alt={pokemon.name}
+            />
+          </div>
+        </header>
+      </div>
+
+      <div className="flex flex-row mt-5 ">
+        <div className="w-1/2 bg-gray-200 py-10 px-5 rounded-xl text-neutral-800">
+          <h2 className="text-2xl mb-2">Stats</h2>
+          <ul className="w-48">
+            {pokemon.stats.map((stat: any) => (
+              <li
+                key={stat.stat.name}
+                className="flex flex-row justify-between w-full"
+              >
+                <p className="capitalize">{stat.stat.name}</p>
+                <p>{stat.base_stat}</p>
+              </li>
+            ))}
+          </ul>
+        </div>
+        <div className="w-1/2 bg-red-400 rounded-lg text-white py-10 px-5">
+          <h2 className="text-2xl mb-2">Abilities</h2>
+          <ul className="w-48">
+            {pokemon.abilities.map((ability: any) => (
+              <li
+                key={ability.ability.name}
+                className="flex flex-row justify-between w-full"
+              >
+                <p className="capitalize">{ability.ability.name}</p>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
+    </section>
+  );
 }
 export default Pokemon;
